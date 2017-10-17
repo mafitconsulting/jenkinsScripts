705de89b4bc426cfe14aa5d4291d24c9320a1d1e
root <root@fieldhousem3.mylabserver.com>
2017-10-17 17:48:58 +0000

    updated script

--- jenkinsPreReqs.sh
+++ jenkinsPreReqs.sh
@@ -3,9 +3,16 @@
 
 RCLOCAL="/etc/rc.local"
 JAVA_VERSION=$1
+TYPE=$2
+
+if [ "$#" != "2" ]; then
+  echo "USAGE: jenkinsPreReqs.sh <JRE RPM> <MASTER|SLAVE>
+  exit 1
+fi
 
 # Install Java rpm
 
+echo "Installing Java.. please wait.."
 rpm -Uvh "$JAVA_VERSION" > /dev/null 2>&1
 
 if [ "$?" -eq "0" ]; then
@@ -13,15 +20,20 @@ if [ "$?" -eq "0" ]; then
    alternatives --install /usr/bin/java java /usr/java/latest/bin/java 200000
    alternatives --install /usr/bin/javac javac /usr/java/latest/bin/javac 200000
    alternatives --install /usr/bin/jar jar /usr/java/latest/bin/jar 200000
-   echo "updating rc.local..."
+   echo "Updating rc.local..."
    echo "export JAVA_HOME=\"/usr/java/latest\"" >> $RCLOCAL
    if [ "$?" -eq "0" ]; then
+      echo "Successully updated rc.local"
       # Add Jenkins user
       echo "Adding Jenkins user...."
       useradd -d /var/lib/jenkins jenkins
       if getent passwd jenkins > /dev/null 2>&1; then
-         echo "setting up ssh dir for key"
-         su - jenkins -c "mkdir /var/lib/jenkins/.ssh"
+         echo "Successfully created Jenkins user"
+         if [ "$TYPE" -eq "SLAVE" ]; then
+            echo "Setting up ssh dir for authorized_keys on slave""
+            su - jenkins -c "mkdir /var/lib/jenkins/.ssh"
+            su - jenkins -c "touch /var/lib/jenkins/.ssh/authorized_keys"
+         fi
       else
          echo "Problem creating Jenkins users"
          exit 3
@@ -29,6 +41,7 @@ if [ "$?" -eq "0" ]; then
    else
       echo "Could not update $RCLOCAL"
       exit 4
+   fi
 else
    echo "Problem installing java..."
    exit 5
